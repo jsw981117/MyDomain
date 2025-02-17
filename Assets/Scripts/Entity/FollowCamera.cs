afc53747acd3dbf5808a1b419de1a16011578662
jsw981117 <jsw981117@naver.com>
2025-02-17 20:49:23 +0900

    feat: Add camera movement restrictions

--- Assets/Scripts/Entity/FollowCamera.cs
+++ Assets/Scripts/Entity/FollowCamera.cs
@@ -6,8 +6,12 @@ public class FollowCamera : MonoBehaviour
 {
     public Transform target;
     float offsetX, offsetY;
-    public float smoothSpeed = 0.75f;
+    public float smoothSpeed = 0.75f; // 카메라 이동 속도
     public float followThreshold = 2.0f; // 타겟과 카메라의 거리 임계값
+
+    // 카메라 이동 제한 영역
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
 
     private void Start()
     {
@@ -25,16 +29,22 @@ public class FollowCamera : MonoBehaviour
         Vector3 desiredPosition = new Vector3(target.position.x + offsetX, target.position.y + offsetY, transform.position.z);
         float distance = Vector3.Distance(transform.position, desiredPosition);
 
-        if (distance > followThreshold)
+        if (distance > followThreshold) // target과 카메라 중심의 거리가 멀어지면
         {
-            float dynamicSmoothSpeed = smoothSpeed * (distance / followThreshold); // 거리 비례 속도 조절
+            float dynamicSmoothSpeed = smoothSpeed * (distance / followThreshold); // 거리 비례 속도 조절(멀수록 빨라진다)
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, dynamicSmoothSpeed * Time.deltaTime);
             transform.position = smoothedPosition;
         }
-        else
+        else // 두 개체 간의 거리가 가까울 때 (보통 플레이어가 멈춰 있을 때)
         {
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
             transform.position = smoothedPosition;
         }
+
+        // 카메라 위치 제한
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, minBounds.x, maxBounds.x),
+            Mathf.Clamp(transform.position.y, minBounds.y, maxBounds.y),
+            transform.position.z);
     }
 }
