cffcbabf75891e2986d42618e70527824fc4f94e
jsw981117 <jsw981117@naver.com>
2025-02-17 16:17:58 +0900

    feat: add Player Movement

---
+++ Assets/Scripts/Entity/AnimationHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    private static readonly int IsMove = Animator.StringToHash("IsMove");
+
+    protected Animator anim;
+
+    protected virtual void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public void MoveAnim(Vector2 obj)
+    {
+        anim.SetBool(IsMove, obj.magnitude > .5f);
+    }
+}
---
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    protected Rigidbody2D rb;
+
+    [SerializeField] private SpriteRenderer chRenderer;
+
+    protected Vector2 moveDirection = Vector2.zero;
+    public Vector2 MoveDirection { get { return moveDirection; } }
+
+    protected AnimationHandler animHandler;
+
+    
+
+    public void Init(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+    }
+    
+    protected virtual void FixedUpdate()
+    {
+        Move(moveDirection);
+    }
+    protected virtual void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animHandler = GetComponent<AnimationHandler>();
+    }
+
+    protected virtual void Move(Vector2 direction)
+    {
+        direction = direction * 3;
+        rb.velocity = direction;
+        animHandler.MoveAnim(direction);
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        moveDirection = inputValue.Get<Vector2>();
+        moveDirection = moveDirection.normalized;
+
+        if (moveDirection.x < 0)
+        {
+            chRenderer.flipX = true;
+        }
+        if (moveDirection.x > 0)
+        {
+            chRenderer.flipX = false;
+        }
+    }
+}
